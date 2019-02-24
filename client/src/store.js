95bd34206f0acd86cf812e68a7abfe247b753f66
nikitalaletin <nikita.laletin@x2sy.com>
2019-02-24 14:36:17 +0200

    fix bracket\

--- client/src/store.js
+++ client/src/store.js
@@ -15,6 +15,7 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     //Opera extension setup
     //window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose)
+  )
 );
 
 export default store;
